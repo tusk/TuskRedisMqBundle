8862937aa48d2c5cee7dcf7c4cd6e1a9155b2c91
Bernard van Niekerk <frizzy@paperjaw.com>
2012-05-02 15:59:18 +0100

    Added missing options

--- Command/ConsumerCommand.php
+++ Command/ConsumerCommand.php
@@ -29,7 +29,8 @@ class ConsumerCommand extends ContainerAwareCommand
             ->setDescription('TuskRedisMq consumer command')
             ->addArgument('consumer', InputArgument::REQUIRED, 'Consumer name')
             ->addOption('messages', 'm', InputOption::VALUE_OPTIONAL, 'Messages to consume', 0)
-            ->addOption('listen', 'l', InputOption::VALUE_NONE, 'Start in paused mode. Send SIGCONT to start', null);
+            ->addOption('listen', 'l', InputOption::VALUE_OPTIONAL, 'Listen timeout in seconds', 10)
+            ->addOption('paused', 'p', InputOption::VALUE_NONE, 'Start in paused mode. Send SIGCONT to start');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
